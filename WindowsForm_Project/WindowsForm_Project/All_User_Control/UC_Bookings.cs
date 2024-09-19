e21fe078c694dc52885e8d9ea3c36a3c96924951
Zannnnz <nguyenvovanio44@gmail.com>
2024-09-19 14:35:17 +0700

    er

--- WindowsForm_Project/WindowsForm_Project/All_User_Control/UC_Bookings.cs
+++ WindowsForm_Project/WindowsForm_Project/All_User_Control/UC_Bookings.cs
@@ -145,7 +145,10 @@ namespace WindowsForm_Project.All_User_Control
                 }
             }
         }
-
+        /// <summary>
+        /// //////
+        /// </summary>
+        /// <returns></returns>
         private bool ValidateInput_Cus()
         {
             if (txtcccd_cus.Text == "" || txtfirstname_cus.Text == "" || txtlastname_cus.Text == "" || txtsdt_cus.Text == "" || txtemail_cus.Text == "" || txtgioitinh_cus.SelectedItem == null || txtngaysinh_cus.Value == null)
